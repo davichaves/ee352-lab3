18e6ac6b98c0eada2bc286d2a3631b4a60a63539
Davi Chaves <dchaves@usc.edu>
2015-10-01 18:38:58 -0700

    improving sort

--- DaviRodriguesChaves_LAB3.asm
+++ DaviRodriguesChaves_LAB3.asm
@@ -49,53 +49,42 @@ Cont1:	lw $t1, ($t0)	 # Load the value at $t0 into $t1.
 	bltz $t1, ExitPrintArray # Exit the loop.
 	
 	add $a0,$t1,$0	# Move $t1 to $a0.
-    	li $v0, 1      	# Print integer.              
-    	syscall
+    li $v0, 1      	# Print integer.              
+    syscall
 
-    	la $a0, space	# Load a space:  " ".
-    	li $v0, 4     	# Print string.               
-    	syscall
+    la $a0, space	# Load a space:  " ".
+    li $v0, 4     	# Print string.               
+    syscall
 	
 	addi $t0,$t0,4	# Increment the address of 'theArray' by 4.
 	j Cont1 	# Jump to 'Cont1'.
 	
-    	ExitPrintArray:	jr $ra		#return
+ExitPrintArray:	jr $ra		#return
 
 BubbleSort:
-	#########################
-	# WRITE YOUR CODE HERE!	#
-	#########################
-	la	$t0, theArray     # move address of the array into $t0
-	li      $s0, 1      # boolean swap = false.  0 --> false, 1 --> true
-	li      $t1, 0      # j = 0;
-	li      $t2, 0      # i = 0;
-	li      $s1, 9      # array length
+	la 		$t0, theArray	# move address of theArray into $t0
+	li      $s0, 1			# boolean swap = true.  0 --> false, 1 --> true
+	li      $t2, 0			# i = 0;
 loop:
-	beqz    $s0, exit       # exit if swap = false
-	li      $s0, 0          # swap = false;
-	addiu   $t1, $t1, 1  # j++;
-	move    $t2, $0      # i = 0;
-	subu    $s2, $s1, $t1  # s2 = length - j
+	li      $s0, 0			# swap = false;
+	move    $t2, $0			# i = 0;
+	subu    $s2, $s1, $t1	# s2 = length - j
 forLoop:
-	bge     $t2, $s2, exitForLoop   # if i>=s2, exit
-	lw      $a0, 0($t0)         # a0 = array[i]
-	lw      $a1, 4($t0)         # a1 = array[i+1]
+	bge     $t2, $s2, exitForLoop	# if i>=s2, exit
+	lw      $a0, 0($t0)		# a0 = array[i]
+	lw      $a1, 4($t0)		# a1 = array[i+1]
 	ble     $a0, $a1, update        # if array[i]<=array[i+1] skip
-	sw      $a1, 0($t0)         # a[i+1] = a[i]
-	sw      $a0, 4($t0)         # a[i] = a[i+1]
-	li      $s0, 1                 # swap = true;
+	sw      $a1, 0($t0)		# a[i+1] = a[i]
+	sw      $a0, 4($t0)		# a[i] = a[i+1]
+	li      $s0, 1			# swap = true;
 update:
-	addiu   $t2, $t2, 1         # i++
-	sll     $t3, $t2, 2         # t3 = i*4
-	addu    $t0, $t0, $t3        # point to next element -->
+	addiu   $t2, $t2, 1		# i++
+	sll     $t3, $t2, 2		# t3 = i*4
+	addu    $t0, $t0, $t3	# point to next element -->
 	j       forLoop
 exitForLoop:
-	j   loop
-exit:
+	bnez    $s0, loop		# exit if swap = true
 	jr      $ra
-	#########################
-	# WRITE YOUR CODE HERE!	#
-	#########################
 
 
 pushStack:
